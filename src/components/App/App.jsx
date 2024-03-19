cf1c53c3531b7349cb7985743481cb4d93b52e33
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-03-19 19:23:46 +0200

    старт завдання 5

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,105 +1,38 @@
-import "./App.module.css";
-import { useEffect, useState } from "react";
-import SearchBar from "../SearchBar/SearchBar";
-import ImageGallery from "../ImageGallery/ImageGallery";
-import Loader from "../Loader/Loader";
-import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
-import ImageModal from "../ImageModal/ImageModal";
-import { getFetchImg } from "../../services/api";
+import { NavLink, Route, Routes } from "react-router-dom";
+import clsx from "clsx";
+import css from "./App.module.css";
+import HomePage from "../../pages/HomePage";
+import MoviesPage from "../../pages/MoviesPage";
+import MovieDetailsPage from "../../pages/MovieDetailsPage";
+import MovieCast from "../../pages/MovieCast";
+import MovieReviews from "../../pages/MovieReviews";
+import NotFoundPage from "../../pages/NotFoundPage";
+
+const getNavLincClassNames = ({ isActive }) =>
+  clsx(css.headerLinc, { [css.active]: isActive });
 
 function App() {
-  const [page, setPage] = useState(1);
-  const [query, setSearchQuery] = useState("");
-  const [imageGallery, setImageGallery] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [showBtn, setShowBtn] = useState(false);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [imageModalData, setImageModalData] = useState({
-    imgSrc: "",
-    imgDescription: "",
-    imgAltDescription: "",
-  });
-
-  const per_page = 12;
-
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-    },
-  };
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
-
-  useEffect(() => {
-    if (!query) return;
-    async function fetchImages() {
-      try {
-        setIsLoading(true);
-        setIsError(false);
-        const data = await getFetchImg(page, per_page, query);
-        setImageGallery((prevImages) => [...prevImages, ...data.results]);
-
-        setShowBtn(page < data.total_pages);
-      } catch (err) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-        // setShowBtn(true);
-        if (page > 1) {
-          window.scrollBy({
-            top: window.innerHeight - 200,
-            left: 0,
-            behavior: "smooth",
-          });
-        }
-      }
-    }
-    fetchImages();
-  }, [query, page, per_page]);
-
-  const onSetSearchQuery = (query) => {
-    setSearchQuery(query);
-    setPage(1);
-    setImageGallery([]);
-  };
-  const onNextPage = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
-  const handleImageClick = (img) => {
-    setImageModalData(img);
-    openModal();
-  };
-
   return (
     <div>
-      <SearchBar onSetSearchQuery={onSetSearchQuery} />
-      {isError && <ErrorMessage />}
+      <header className={css.header}>
+        <NavLink className={getNavLincClassNames} to="/">
+          Home
+        </NavLink>
+        <NavLink className={getNavLincClassNames} to="/movies">
+          Movies
+        </NavLink>
+      </header>
 
-      <ImageGallery
-        imageGallery={imageGallery}
-        handleImageClick={handleImageClick}
-      />
-      {isLoading && <Loader />}
-      <ImageModal
-        style={customStyles}
-        closeModal={closeModal}
-        modalIsOpen={modalIsOpen}
-        {...imageModalData}
-      />
-      {showBtn && <LoadMoreBtn onLoadMore={onNextPage} />}
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+          <Route path="/movies/:movieId/cast" element={<MovieCast />} />
+          <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
+          <Route path="/" element={<NotFoundPage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/pages/MovieCast.jsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const MovieCast = () => {
+  const { cast } = useParams;
+  return (
+    <div>
+      MovieCast
+      <p>Prod id: {cast}</p>;
+    </div>
+  );
+};
+
+export default MovieCast;
